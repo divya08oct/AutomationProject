cfbd6ed7b71a28012aa86b32cdd4c1142581f54b
Divya Srivastava <divya.srivastava01@nagarro.com>
2023-09-17 22:54:50 +0530

    My First Commit

---
+++ src/BrowserTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
+
+public class BrowserTest {
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void getData() {
+		System.out.println("Hello");
+		//System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://divyasriacademy.azurewebsites.net/webapp/");
+		String currentTitle = driver.findElement(By.cssSelector("body h1")).getText();
+		System.out.println("Currenyt title is : "+currentTitle);
+		Assert.assertEquals(currentTitle,"Divya Srivastava.com Learning");
+		driver.close();
+	}
+}
